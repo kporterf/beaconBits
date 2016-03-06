cafd112eba53d268d4400d9baefee0b0abca8168
Katie Porterfield <kporterf@stetson.edu>
2016-03-06 06:02:49 -0500

    updated

--- strobe.js
+++ strobe.js
@@ -16,50 +16,55 @@ var visibleNetworks = null;
 var ping = null;
 var up = null;
 var down = null;
+var start = null;
 
 board.on("ready", function() {
 	// Create an Led on pin 13
 	//var led = new five.Led(13);
 	
-	//push to database
-	function pushData() {
-		request.post("http://52.91.76.141/api/beacons", function optionalCallback(err, httpResponse, body) {
-  			if (err) {
-    				return console.error('upload failed:', err);
-  			}
-  			console.log('Upload successful!  Server responded with:', body);
-		}).form({"serial":serial, "details":details, "gps":gps, "channel":channel, "ip":ip, "mac":mac, "signal_strength":signalStrength, "visible_networks":visibleNetworks, "ping":ping, "up":up, "down":down});
-	}
+		board.loop(5000,function(){run();});
+		//push to database
+		function pushData() {
+			request.put("http://52.91.76.141/api/beacons/1", function optionalCallback(err, httpResponse, body) {
+				if (err) {
+					return console.error('upload failed:', err);
+				}
+				console.log('Upload successful!  Server responded with:', body);
+			}).form({"serial":serial, "details":details, "gps":gps, "channel":channel, "ip":ip, "mac":mac, "signal_strength":signalStrength, "visible_networks":visibleNetworks, "ping":ping, "up":up, "down":down});
+		}
 
-	function download() {
-		var start2 = Date.now();
-		var url = "https://www.google.com";
-		request(url, function (error, response, body) {
-                    	if (!error && response.statusCode == 200) {
-                            console.log("downloaded Google"); // Show the HTML for the Modulus homepage.
-			    down = Date.now() - start2;
-			    pushData();
-                        }
-                });
-	}	
+		function download() {
+			var start2 = Date.now();
+			var url = "https://www.google.com";
+			request(url, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+				    console.log("downloaded Google"); // Show the HTML for the Modulus homepage.
+				    down = Date.now() - start2;
+				    pushData();
+				}
+			});
+		}	
 
-	// Strobe the pin on/off, defaults to 100ms phases
-	function puts(error, stdout, stderr) {
-		ping = Date.now() - start;
-		ping = ping/1000;
-		console.log("ping: ", ping);
-		//led.strobe();
-		download();
-	}
-	
-	var start = Date.now();
-	exec("ping -c 3 google.com", puts); 
-
-	/*fetch('https://github.com/').then(function(res) {
-		return res.text();
-		}).then(function(body) {
-		//led.strobe();
-		console.log(body);
-	});*/
+		// Strobe the pin on/off, defaults to 100ms phases
+		function puts(error, stdout, stderr) {
+			ping = Date.now() - start;
+			ping = ping/1000;
+			console.log("ping: ", ping);
+			//led.strobe();
+			download();
+		}
+		
+		function run()
+		{
+			start = Date.now();
+			exec("ping -c 3 google.com", puts); 
+		
+		}
+		/*fetch('https://github.com/').then(function(res) {
+			return res.text();
+			}).then(function(body) {
+			//led.strobe();
+			console.log(body);
+		});*/
 });
 
